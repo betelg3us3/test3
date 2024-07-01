b3e58d798186fbce695ffd736aed48a826c890dd
betelg3us3 <alisaebancuel@gmail.com>
2024-07-01 08:21:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction2(){
+  window.location.replace("https://food.grab.com/ph/en/restaurant/turks-168-shopping-mall-5th-floor-delivery/2-CYUJJNUKVJMVG6?");
+}
+
+function myFunction() {
+    let text = "Are you sure you want to submit?";
+  if (confirm(text) == true) {
+   alert("The message has been sent!")
+  } else {
+    alert("The message has been cancelled!")
+  }
+  document.getElementById("submit").innerHTML;
+}
+
